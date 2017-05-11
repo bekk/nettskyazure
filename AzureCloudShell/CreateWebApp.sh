c0dab616ef4a2095e525d8d549f0e516d2d09c8b
Nordine Ben Bachir <nordineb@users.noreply.github.com>
2017-05-11 13:44:36 +0200

    Update CreateWebApp.sh

--- AzureCloudShell/CreateWebApp.sh
+++ AzureCloudShell/CreateWebApp.sh
@@ -4,7 +4,7 @@ webappname=nordinewebapp
 serviceplan=nordineplan
 
 az group create --location westeurope --name $resourcegroupname
-az appservice plan create --name $nordineplan --resource-group $resourcegroupname --sku FREE
+az appservice plan create --name $serviceplan --resource-group $resourcegroupname --sku FREE
 az webapp create --name $webappname --resource-group $resourcegroupname --plan $serviceplan
 az webapp deployment source config --name $webappname --resource-group $resourcegroupname --repo-url $gitrepo --branch master --manual-integration
 #az group delete --name $resourcegroupname --no-wait --yes
